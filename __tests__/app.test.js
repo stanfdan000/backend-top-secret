81ba231a0ec9149167b3cc21b0da3b8b464b4910
sirdan <stanford303@gmail.com>
2022-08-10 16:00:49 -0700

    made files for my routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,9 +7,18 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+
+
+
+
+
+
+
+
+
+
+  
+  
   afterAll(() => {
     pool.end();
   });
